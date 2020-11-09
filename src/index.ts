610889ba86028c7a4e3c71ddbabfe6e89cbccabf
Kyle Nazario <kyle@servicecore.com>
2020-11-09 10:41:34 -0700

    .first on Array

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-interface Array<T> {
-  readonly first: T | undefined;
-  readonly last: T | undefined;
+export {};
+
+declare global {
+  interface Array<T> {
+    readonly first: T | undefined;
+    readonly last: T | undefined;
+    readonly count: number;
+  }
 }
 
 Object.defineProperty(Array.prototype, 'first', {
-  get(): any | undefined {
+  get(this: Array<any>): number | undefined {
     if (this.length > 0) return this[0];
     else return undefined;
-  }
+  },
+  configurable: true
 });
-
-const x: string[] = ['1', '2'];
-const y: string[] = [];
-console.log(x.first, y.first);
