8f3069d1567ad43c2b55e19bfb4fa14c717c56d7
Kyle Nazario <kyle@servicecore.com>
2020-11-09 13:48:47 -0700

    String helpers

--- src/String.ts
+++ src/String.ts
@@ -4,6 +4,9 @@ declare global {
   interface String {
     readonly isEmpty: boolean;
     readonly inserted: (substring: string, at: number) => string;
+    readonly first: string | undefined;
+    readonly last: string | undefined;
+    readonly randomElement: () => string | undefined;
   }
 }
 
@@ -16,4 +19,25 @@ Object.defineProperty(String.prototype, 'isEmpty', {
 // @ts-ignore
 String.prototype['inserted'] = function (substring: string, at: number) {
   return this.slice(0, at) + substring + this.slice(at + 1);
+}
+
+Object.defineProperty(String.prototype, 'first', {
+  get(this: String) {
+    if (this.length > 0) return this[0];
+    else return undefined;
+  }
+});
+
+Object.defineProperty(String.prototype, 'last', {
+  get(this: String) {
+    if (this.length > 0) return this[this.length - 1];
+    else return undefined;
+  }
+});
+
+// @ts-ignore
+String.prototype['randomElement'] = function () {
+  if (this.length < 1) return undefined;
+  const index = Math.floor(Math.random() * this.length);
+  return this[index];
 }
