87622a28534fb003cf6b2028ed96e67c6fc8d269
Kyle Nazario <kyle@servicecore.com>
2020-11-10 14:04:53 -0700

    Defines properties on string with built-in fn

--- src/String.ts
+++ src/String.ts
@@ -18,96 +18,110 @@ declare global {
   }
 }
 
-Object.defineProperty(String.prototype, 'isEmpty', {
-  get(this: String) {
-    return this.valueOf().length === 0;
+Object.defineProperties(String.prototype, {
+  isEmpty: {
+    get(this: String) {
+      return this.valueOf().length === 0;
+    }
+  },
+  inserted: {
+    get(this: String) {
+      return (substring: string, at: number) => {
+        return this.slice(0, at) + substring + this.slice(at);
+      };
+    }
+  },
+  first: {
+    get(this: String) {
+      if (this.length > 0) return this[0];
+      else return undefined;
+    }
+  },
+  last: {
+    get(this: String) {
+      if (this.length > 0) return this[this.length - 1];
+      else return undefined;
+    }
+  },
+  randomElement: {
+    get(this: String) {
+      return () => {
+        if (this.length < 1) return undefined;
+        const index = Math.floor(Math.random() * this.length);
+        return this[index];
+      };
+    }
+  },
+  map: {
+    get(this: String) {
+      return (callback: (char: string, index?: number, parent?: string) => string) => {
+        let mappedString = '';
+        const s = this.valueOf();
+        for (let i = 0; i < this.length; i++) {
+          mappedString += callback(s[i]);
+        }
+        return mappedString;
+      };
+    }
+  },
+  compactMap: {
+    get(this: String) {
+      return (callback: (char: string, index?: number, parent?: string) => string | undefined) => {
+        let mappedString = '';
+        const s = this.valueOf();
+        for (let i = 0; i < this.length; i++) {
+          const mappedChar = callback(s[i]);
+          if (typeof mappedChar === 'string') mappedString += mappedChar;
+        }
+        return mappedString;
+      };
+    }
+  },
+  forEach: {
+    get(this: String) {
+      return (callback: (char: string, index?: number, parent?: string) => void) => {
+        const s: string = this.valueOf();
+        for (let i = 0; i < this.length; i++) {
+          callback(s[i]);
+        }
+      };
+    }
+  },
+  startIndex: {
+    get(this: String) {
+      if (this.length > 0) return 0;
+      else return undefined;
+    }
+  },
+  endIndex: {
+    get(this: String) {
+      if (this.length > 0) return this.length - 1;
+      else return undefined;
+    }
+  },
+  sorted: {
+    get(this: String) {
+      return () => {
+        return this.split('').sort().join('');
+      };
+    }
+  },
+  reversed: {
+    get(this: String) {
+      return () => {
+        let reversed = '';
+        for (let i = this.endIndex ?? -1; i >= 0; i--) {
+          reversed += this[i];
+        }
+        return reversed;
+      }
+    }
+  },
+  shuffled: {
+    get(this: String) {
+      return () => {
+        return this.split('').shuffled().join('')
+      }
+    }
   }
 });
-
-// @ts-ignore
-String.prototype['inserted'] = function (substring: string, at: number) {
-  return this.slice(0, at) + substring + this.slice(at + 1);
-}
-
-Object.defineProperty(String.prototype, 'first', {
-  get(this: String) {
-    if (this.length > 0) return this[0];
-    else return undefined;
-  }
-});
-
-Object.defineProperty(String.prototype, 'last', {
-  get(this: String) {
-    if (this.length > 0) return this[this.length - 1];
-    else return undefined;
-  }
-});
-
-// @ts-ignore
-String.prototype['randomElement'] = function () {
-  if (this.length < 1) return undefined;
-  const index = Math.floor(Math.random() * this.length);
-  return this[index];
-}
-
-// @ts-ignore
-String.prototype['map'] = function (callback: (char: string, index?: number, parent?: string) => string) {
-  let mappedString = '';
-  const s = this.valueOf();
-  for (let i = 0; i < this.length; i++) {
-    mappedString += callback(s[i]);
-  }
-  return mappedString;
-}
-
-// @ts-ignore
-String.prototype['compactMap'] = function (callback: (char: string, index?: number, parent?: string) => string | undefined) {
-  let mappedString = '';
-  const s = this.valueOf();
-  for (let i = 0; i < this.length; i++) {
-    const mappedChar = callback(s[i]);
-    if (typeof mappedChar === 'string') mappedString += mappedChar;
-  }
-  return mappedString;
-}
-
-// @ts-ignore
-String.prototype['forEach'] = function (callback: (char: string, index?: number, parent?: string) => void) {
-  const s: string = this.valueOf();
-  for (let i = 0; i < this.length; i++) {
-    callback(s[i]);
-  }
-}
-
-Object.defineProperty(String.prototype, 'startIndex', {
-  get(this: String) {
-    if (this.length > 0) return 0;
-    else return undefined;
-  }
-});
-
-Object.defineProperty(String.prototype, 'endIndex', {
-  get(this: String) {
-    if (this.length > 0) return this.length - 1;
-    else return undefined;
-  }
-});
-
-// @ts-ignore
-String.prototype['sorted'] = function () {
-  return this.split('').sort().join('');
-}
-
-// @ts-ignore
-String.prototype['reversed'] = function () {
-  let reversed = '';
-  for (let i = this.endIndex ?? -1; i >= 0; i--) {
-    reversed += this[i];
-  }
-  return reversed;
-}
-
-// @ts-ignore
-String.prototype['shuffled'] = function () {
-  return this.split('').shuffled().join('')
-}
