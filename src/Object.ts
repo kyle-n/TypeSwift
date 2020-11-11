a3feb372146e4eeab744f00ed3384b890608d749
Kyle Nazario <kyle@servicecore.com>
2020-11-10 17:10:11 -0700

    Object extensions

---
+++ src/Object.ts
@@ -0,0 +1,45 @@
+export {};
+
+declare global {
+  interface Object {
+    readonly isEmpty: boolean;
+    readonly count: number;
+    readonly first: any;
+    readonly randomElement: () => any;
+    readonly merge: (objectToMerge: Object, uniquingKeysWith: (valueOne: any, valueTwo: any) => any) => Object;
+  }
+}
+
+Object.defineProperties(Object.prototype, {
+  isEmpty: {
+    get(this: Object) {
+      return Object.keys(this).length === 0;
+    }
+  },
+  count: {
+    get(this: Object) {
+      return Object.keys(this).length;
+    }
+  },
+  first: {
+    get(this: Object) {
+      return Object.values(this)[0];
+    }
+  },
+  randomElement: {
+    get(this: Object) {
+      const index = Math.floor(Math.random() * Object.values(this).length);
+      return Object.values(this)[index];
+    }
+  },
+  // merge: {
+  //   get(this: Object) {
+  //     return (objectToMerge: Object, uniquingKeysWith: (valueOne: any, valueTwo: any) => any): Object => {
+  //       const merged = {...this} as any;
+  //       Object.keys(objectToMerge).forEach(key => {
+  //         if (merged.hasOwnProperty(key)) merged[key] = uniquingKeysWith(merged[key], objectToMerge[key] as any);
+  //       });
+  //     };
+  //   }
+  // }
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './Array';
 export * from './Number';
 export * from './String';
+export * from './Object';
