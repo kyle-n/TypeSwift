056187ba71053abb0ac07d5a6f61492fcbeaba02
Kyle Nazario <kyle@servicecore.com>
2020-11-11 10:07:06 -0700

    Random bool

--- src/Boolean.ts
+++ src/Boolean.ts
@@ -2,6 +2,13 @@ export {};
 
 declare global {
   interface Boolean {
-
   }
 }
+
+Object.defineProperties(Boolean, {
+  random: {
+    get() {
+      return (): boolean => Math.random() >= 0.5;
+    }
+  }
+});
