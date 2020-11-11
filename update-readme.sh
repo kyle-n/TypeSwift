495dcbda5727ed02a5cbd01936cb7fe10d9078c2
Kyle Nazario <kyle@servicecore.com>
2020-11-11 15:57:36 -0700

    Script loop working

--- update-readme.sh
+++ update-readme.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 newline=$'\n'
 
 # Remove previous content
@@ -7,17 +8,20 @@ sed -i '/^###\sFeatures/,/^#/{/^#/!d}' README.md
 features_line_number=$(grep -n '### Features' README.md | cut -d: -f 1)
 features_line_number=$((features_line_number+1))
 
-# Object output
-object_output=$(sed -n '/^export {}/,/^Object\.defineProperties/p;/^Object\.defineProperties/q' src/Object.ts | tail -n +2 | head -n -2)
-obj=$(echo -e "${object_output}\n" | sed ':a $!{N; ba}; s/\n/\\n/g')
-
-# Blank lines around section
-sed -i "${features_line_number}i ${newline}" README.md
-sed -i "${features_line_number}i ${newline}" README.md
-next_line=$((features_line_number+1))
-
-# Wrap header declaration and insert into README
 code_header='```typescript'
 code_footer='```'
-insertion=$(sed -i "${next_line}i ${code_header}${obj}${code_footer}" README.md)
-#cat README.md
+sed -i "${features_line_number}i ${newline}" README.md
+
+for file in 'src/Object.ts' 'src/Array.ts'
+do
+  # Blank lines around section
+  sed -i "${features_line_number}i ${newline}" README.md
+  next_line=$((features_line_number+1))
+
+  # file output
+  content=$(sed -n '/^export {}/,/^Object\.defineProperties/p;/^Object\.defineProperties/q' $file | tail -n +2 | head -n -2)
+  formatted_content=$(echo -e "${content}\n" | sed ':a $!{N; ba}; s/\n/\\n/g')
+
+  # Wrap header declaration and insert into README
+  insertion=$(sed -i "${next_line}i ${code_header}${formatted_content}${code_footer}" README.md)
+done
