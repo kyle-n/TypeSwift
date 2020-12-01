a091fdcefa8cb9de150c9ef1f71f0c73329bc4a4
Kyle Nazario <kyle@servicecore.com>
2020-12-01 15:22:22 -0700

    Tests new Boolean/Number static methods

--- src/Number.test.ts
+++ src/Number.test.ts
@@ -1,19 +1,23 @@
 import './index';
 
-test('Number.quotientAndRemainder()', () => {
+test('Number.prototype.quotientAndRemainder()', () => {
   const n = 42;
   expect(n.quotientAndRemainderOf(10)).toEqual([4, 2]);
 });
 
-test('Number.isMultipleOf()', () => {
+test('Number.prototype.isMultipleOf()', () => {
   const n = 10;
   expect(n.isMultipleOf(2)).toBe(true);
   expect(n.isMultipleOf(3)).toBe(false);
 });
 
-test('Number.isZero', () => {
+test('Number.prototype.isZero', () => {
   const one = 1;
   const zero = 0;
   expect(one.isZero).toBe(false);
   expect(zero.isZero).toBe(true);
+});
+
+test('Number.zero', () => {
+  expect(Number.zero).toBe(0);
 });
